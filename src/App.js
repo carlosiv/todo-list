5d24300e01d18a042473ae62e557c3e9d2d0bc69
carlos <cvelasco@skourse.com>
2022-01-21 11:34:11 +0800

    Done Exercise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { v4 as uuidv4 } from 'uuid';
+import TodoList from "./components/TodoList";
+import DoneTodo from "./components/DoneTodo";
+import TodoDashboard from "./components/TodoDashboard";
+import TodoForm from "./components/TodoForm";
 
 function App() {
+  const [newTask, setNewTask] = useState('');
+  const [newTaskDue, setNewTaskDue] = useState(Date.now());
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      todo: "First Task",
+      status: "Current",
+      createdAt: 1642458275288,
+      finishedAt: '',
+      due: 1642458275288,
+    },
+    {
+      id: 2,
+      todo: "Second Task",
+      status: "Unfinished",
+      createdAt: 1642458275388,
+      finishedAt: '',
+      due: 1642458275288,
+    },
+    {
+      id: 3,
+      todo: "Third Task",
+      status: "Done",
+      finishedAt: 1642730689450,
+      createdAt: 1642458275388,
+      due: 1642458275288,
+    },
+    {
+      id: 4,
+      todo: "Fourth Task",
+      status: "Done",
+      finishedAt: 1642730689450,
+      createdAt: 1642458275388,
+      finishedAt: 1642458275288,
+      due: 1642458275288,
+    },
+  ]);
+
+
+ 
+  function addTask(e) {
+    e.preventDefault()
+    if (newTask != '') {
+      setNewTask('')
+    setNewTaskDue(Date.now())
+    setTodos([...todos,{id: uuidv4(), todo:newTask, status:"Unfinished",createdAt: Date.now(),  due:newTaskDue}])
+    } else {
+      alert('Please fill task')
+    }
+  }
+
+  function setStatus(id,status){
+    todos
+      .filter((todo) => todo.id === id)
+      .map((task) => {
+        task.status = status
+        task.finishedAt = Date.now()
+        //to re-render component
+        setTodos([...todos])
+      });
+
+  }
+
+  function deleteTodo(id){
+   const tasks = todos.filter((todo) => todo.id != id)
+   setTodos([...tasks])
+  }
+
+  const doneTask = todos.filter(task => task.status === 'Done')
+  const unfinishedTask = todos.filter(task => task.status != 'Done')
+  const onGoingTask = todos.filter(task => task.status === 'Current')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container w-full">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 dark:bg-gray-700">
+        <div className="lg:col-span-8 col-span-1 flex flex-wrap my-2">
+          <TodoList unfinishedTask={unfinishedTask} setStatus={setStatus} deleteTodo={deleteTodo} />
+          <DoneTodo doneTask={doneTask} />
+        </div>
+        <div className="sm:block md:col-span-4 col-span-1 dark:bg-gray-700">
+          <div className="lg:sticky relative mt-5 mr-3 top-8">
+          <TodoDashboard todos={todos} unfinishedTask={unfinishedTask} onGoingTask={onGoingTask} doneTask={doneTask} />
+          <TodoForm newTask={newTask} setNewTask={setNewTask} newTaskDue={newTaskDue} setNewTaskDue={setNewTaskDue} addTask={addTask} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import moment from "moment";
+
+const TodoForm = ({newTask, setNewTask, newTaskDue, setNewTaskDue, addTask}) => {
+  return <div className="mt-5 border-2 shadow-lg rounded-lg bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+  <form className="w-full max-w-xl rounded-lg px-4 pt-2">
+<div className="flex flex-wrap -mx-3 mb-6">
+ <h2 className="px-4 pt-3 pb-2 text-gray-800 text-lg font-bold">Add New Task</h2>
+
+ <div className="w-full md:w-full px-3 mb-2 mt-2">
+    <textarea className="bg-gray-200 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="todo" placeholder='Type the Task' required value={newTask} onChange={(e) => {setNewTask(e.target.value)}}></textarea>
+ </div>
+ <div className="w-full md:w-full px-3 mb-2 mt-2">
+ <label htmlFor="due">Due date:</label>
+    <input className="bg-gray-200 rounded border border-gray-400 leading-normal resize-none w-full py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="due" type="date" required value={moment(newTaskDue).format('YYYY-MM-DD')} onChange={(e) => { setNewTaskDue(e.target.value) }} />
+ </div>
+ <div className="w-full md:w-full flex items-start px-3">
+
+    <div className="-mr-1">
+       <input type='button' className="bg-green-400 text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100" value='Add' onClick={addTask}/>
+    </div>
+ </div>
+ </div>
+</form>
+  </div>
+};
+
+export default TodoForm;
